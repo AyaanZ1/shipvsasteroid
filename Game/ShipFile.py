0c554b238fc4913ea88f0eb277d699e146782ad2
Sawbez <63512091+Sawbez@users.noreply.github.com>
2022-05-28 10:10:03 -0700

    easy fix

--- Game/ShipFile.py
+++ Game/ShipFile.py
@@ -1,18 +1,22 @@
 import pygame
 import sys
-
 pygame.init()
 
-class ship(pygame.sprite.Sprite):
+
+class Ship(pygame.sprite.Sprite):
     def __init__(self):
+        """Initialize the ship and set its starting position."""
         super().__init__()
         self.image = pygame.image.load('./image/ship.png')
-        self.x = 254
-        self.y = 286
-        self.width,self.height = self.image.get_size()
-        self.rect = self.image.get_rect(topleft = (254,286))
+        self.image.convert_alpha()
+        self.x = 255
+        self.y = 290
+        self.width, self.height = self.image.get_size()
+        self.rect = self.image.get_rect()#topleft=(254, 286))
         self.mask = pygame.mask.from_surface(self.image)
-    def move(self):
+
+    def update(self, obstacle):
+        """Update the ship's position based on the currently pressed keys."""
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             self.y -= 10
@@ -20,12 +24,14 @@ class ship(pygame.sprite.Sprite):
             self.y += 10
         self.rect.x = self.x
         self.rect.y = self.y
-    def collision(self,obstacle):
-        obstacles = pygame.sprite.GroupSingle()
-        obstacles.add(obstacle)
-        if pygame.sprite.spritecollide(self,obstacles,False,pygame.sprite.collide_mask):
-            print("Game Over")
-            #sys.exit()
 
-    def update(self):
-        self.move()
+        #if pygame.sprite.spritecollide(self, pygame.sprite.GroupSingle(obstacle), False, pygame.sprite.collide_mask):
+        #    print("Game Over")
+        #    sys.exit()
+        if pygame.sprite.collide_mask(self, obstacle):
+            print("Game Over")
+            sys.exit()
+
+    def draw(self, screen):
+        """Draw the ship on the screen."""
+        screen.blit(self.image, self.rect)
